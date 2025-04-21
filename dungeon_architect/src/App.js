9308a0ebbb9dcd6216ac1a27b2ef623194013d87
Tobias-Bodmer <bodmer98@web.de>
2025-04-21 16:35:21 +0200

    Added dungeon_architect as a regular folder (was submodule)

---
+++ dungeon_architect/src/App.js
@@ -0,0 +1,38 @@
+import React, { createContext, useEffect, useState } from "react";
+import LoginForm from "./Login";
+import StoryEditor from "./Story_Editor";
+import StoryOverview from "./Story_Overview";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faUser } from '@fortawesome/free-solid-svg-icons'
+import './App.css';
+
+export default function LandingPage() {
+  const [show, toggleShow] = useState(false);
+
+  function toggle() {
+    toggleShow((show) => !show);
+    console.log(localStorage.getItem("isLoggedIn") === 'true');
+  }
+
+  return (
+    <>
+      <div id='nav'>
+        <button onClick={toggle}>
+          <FontAwesomeIcon icon={faUser} />
+        </button>
+      </div>
+      {!(localStorage.getItem("isLoggedIn") == 'true') && show ? <LoginForm /> : ""}
+      <div>
+        <h1 className={`headline ${show || (localStorage.getItem("isLoggedIn") == 'true') ? "animate" : ""}`}>
+          <span className="line1">Dungeon</span>{" "}
+          <span className="line2">Architect</span>
+        </h1>
+      </div>
+      <div>
+        {(localStorage.getItem("isLoggedIn") == 'true') && !show ? <StoryEditor /> : ""}
+        {(localStorage.getItem("isLoggedIn") == 'true') && show ? <StoryOverview /> : ""}
+
+      </div>
+    </>
+  );
+}
---
+++ dungeon_architect/src/Login.jsx
@@ -0,0 +1,145 @@
+import React, { useState } from "react";
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [loginEmail, setLoginEmail] = useState("");
+  const [regEmail, setRegEmail] = useState("");
+  const [loginPassword, setLoginPassword] = useState("");
+  const [regPassword, setRegPassword] = useState("");
+  const [repPassword, setRepPassword] = useState("");
+  const [loginError, setLoginError] = useState("");
+  const [regError, setRegError] = useState("");
+
+  const handleLoginSubmit = (e) => {
+    e.preventDefault();
+
+    if (!loginEmail || !loginPassword) {
+      setLoginError("Bitte fülle alle Felder aus!");
+      return;
+    }
+
+    const body = { mail: `${loginEmail}`, password: `${loginPassword}` };
+
+    axios.post('https://localhost:1337/login', body, {
+      withCredentials: true
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          localStorage.setItem("loggedInUser", `${loginEmail}`)
+          localStorage.setItem("isLoggedIn", true)
+          window.location.reload();
+        }
+      }).catch(error => {
+        if (error.response) {
+          setLoginError(error.response.data.error);
+        }
+      });;
+  };
+
+  const handleRegSubmit = (e) => {
+    e.preventDefault();
+
+    if (!regEmail || !regPassword || !repPassword && regPassword == repPassword) {
+      setRegError("Bitte fülle alle Felder aus!");
+      return;
+    }
+
+    const body = { mail: `${regEmail}`, password: `${regPassword}` };
+
+    axios.post('https://localhost:1337/registrate', body, {
+      withCredentials: true
+    })
+      .then((response) => {
+        if (response.status == 200) {
+          localStorage.setItem("loggedInUser", `${regEmail}`)
+          localStorage.setItem("isLoggedIn", true)
+          window.location.reload();
+        }
+      }).catch(error => {
+        if (error.response) {
+          setRegError(error.response.data.error);
+        }
+      });
+  };
+
+  return (
+    <div className="login-container">
+      <form onSubmit={handleLoginSubmit} className="login-form">
+        {loginError && <p className="error-message">{loginError}</p>}
+
+        <div className="input-group">
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={loginEmail}
+            onChange={(e) => setLoginEmail(e.target.value)}
+            placeholder="Gib deine E-Mail ein"
+            required
+          />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="password">Passwort</label>
+          <input
+            type="password"
+            id="password"
+            value={loginPassword}
+            onChange={(e) => setLoginPassword(e.target.value)}
+            placeholder="Gib dein Passwort ein"
+            required
+          />
+        </div>
+
+        <button type="submit" className="login-button">
+          Einloggen
+        </button>
+      </form>
+      <form onSubmit={handleRegSubmit} className="registration-form">
+        {regError && <p className="error-message">{regError}</p>}
+
+        <div className="input-group">
+          <label htmlFor="email">E-Mail</label>
+          <input
+            type="email"
+            id="email"
+            value={regEmail}
+            onChange={(e) => setRegEmail(e.target.value)}
+            placeholder="Gib deine E-Mail ein"
+            required
+          />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="password">Passwort</label>
+          <input
+            type="password"
+            id="password"
+            value={regPassword}
+            onChange={(e) => setRegPassword(e.target.value)}
+            placeholder="Gib dein Passwort ein"
+            required
+          />
+        </div>
+
+        <div className="input-group">
+          <label htmlFor="repeat-password">Passwort wiederholen</label>
+          <input
+            type="password"
+            id="password"
+            value={repPassword}
+            onChange={(e) => setRepPassword(e.target.value)}
+            placeholder="Wiederhole dein Passwort"
+            required
+          />
+        </div>
+
+        <button type="submit" className="registration-button">
+          Registrieren
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default LoginForm;
